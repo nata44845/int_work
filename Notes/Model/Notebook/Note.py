f7a10576a996a1ef6025d1eb11b7c5ed9dc4b0a4
nata44845 <krppost@yandex.ru>
2023-10-09 21:31:09 +0700

    first commit

---
+++ Notes/Model/Notebook/Note.py
@@ -0,0 +1,23 @@
+import datetime
+
+
+class Note():
+    id = null
+
+    def __init__(self, id, name, date, text):
+        self.id = id
+        self.name = name
+        self.date = date
+        self.
+
+    def getId():
+        return id
+
+    def setId(int id):
+        pass
+
+    def getFullInfo():
+        return id.name.text
+
+    def getDate():
+        return date
